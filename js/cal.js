88e8ec8667e2fd735b447b3313be5ebf2eccc9cc
poldong <poldong2019@gmail.com>
2022-10-28 12:18:32 +0800

    first commit

---
+++ js/cal.js
@@ -0,0 +1,40 @@
+
+// function addition() {
+//     let x = document.getElementById("firstNum").value
+//     let y = document.getElementById("secondNum").value
+//     const sum = parseFloat(x) + parseFloat(y);
+
+//     alert("The sum of x + y is " + sum)
+// }
+
+// function subtraction() {
+//     let x = document.getElementById("firstNum").value
+//     let y = document.getElementById("secondNum").value 
+//     const difference = x - y;
+
+//     alert("The difference of x - y is " + difference)
+// }
+
+function multiplication() {
+    let x = document.getElementById("firstNum").value
+    let y = document.getElementById("secondNum").value
+    const product = x * y*13;
+
+    alert("You will pay "+ product)
+}
+
+function Days() {
+    
+    let y = document.getElementById("day").value
+    const product = y * 24 +" " + "hours";
+
+    alert("WE WILL ENJOY : "+ product)
+}
+
+function range() {
+    
+    let y = document.getElementById("knot").value
+    const product = y * 1.852;
+
+    alert("Kilometer per hour : "+ product)
+}
